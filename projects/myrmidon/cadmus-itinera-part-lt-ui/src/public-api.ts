cc7bb18dbc29aa85e137e39896820720f6d6296f
Myrmex <dfusi@hotmail.com>
2020-10-28 09:54:49 +0100

    DocReferencesPart

--- projects/myrmidon/cadmus-itinera-part-lt-ui/src/public-api.ts
+++ projects/myrmidon/cadmus-itinera-part-lt-ui/src/public-api.ts
@@ -2,6 +2,8 @@
  * Public API Surface of cadmus-itinera-part-lt-ui
  */
 
+export * from './lib/doc-references-part';
+
 export * from './lib/person-events-part';
 export * from './lib/person-events-part/person-events-part.component';
 export * from './lib/bio-event/bio-event.component';
